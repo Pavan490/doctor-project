cf46982e8000172ea2c89ceb30f115a2810f49d0
Pavan <128314742+Pavan490@users.noreply.github.com>
2025-10-08 12:08:51 +0530

    Delete plan-feature-service/plan-feature-service directory

--- plan-feature-service/plan-feature-service/src/main/java/com/longbow/plan_feature_service/Exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.longbow.plan_feature_service.Exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-		
-	public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- plan-feature-service/plan-feature-service/src/main/java/com/longbow/plan_feature_service/Repository/PlanFeatureRepository.java
+++
@@ -1,9 +0,0 @@
-package com.longbow.plan_feature_service.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.longbow.plan_feature_service.Entity.PlanFeature;
-
-public interface PlanFeatureRepository extends JpaRepository<PlanFeature, Integer> {
-
-}
